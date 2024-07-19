e03487bcf50c2a4240e44e4971f33475f4af71a0
Shiv9993 <112641190+Shiv9993@users.noreply.github.com>
2024-07-19 17:57:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,3 @@ import os
 app_base_path = os.getenv("APP_BASE_PATH", default=os.getcwd())
 data_base_path = os.path.join(app_base_path, "data")
 model_file_path = os.path.join(data_base_path, "model.pkl")
-
-# Ensure the directory exists
-os.makedirs(data_base_path, exist_ok=True)
-
-if not os.path.exists(model_file_path):
-    raise FileNotFoundError(f"No such file or directory: '{model_file_path}'")
-
